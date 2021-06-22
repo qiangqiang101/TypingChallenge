3d4e631883d52bd64d3dabc012f1807eba6aac1d
Bartholomew Ho <qiangqiang101@hotmail.com>
2021-06-23 07:37:53 +0800

    s

--- TypingChallenge/My Project/AssemblyInfo.vb
+++ TypingChallenge/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.621.4")>
-<Assembly: AssemblyFileVersion("1.0.621.4")>
+<Assembly: AssemblyVersion("1.0.623.4")>
+<Assembly: AssemblyFileVersion("1.0.623.4")>
